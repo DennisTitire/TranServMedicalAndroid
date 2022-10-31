0244d7af54b658930cdb1a5e17a69bf8789fa69a
DennisTitire <dennistitire@gmail.com>
2022-10-31 19:24:13 +0200

    implemented Calendar Screen

--- app/src/main/java/com/example/transervmedical/navigation/Routes.kt
+++ app/src/main/java/com/example/transervmedical/navigation/Routes.kt
@@ -1,12 +1,15 @@
 package com.example.transervmedical.navigation
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.transervmedical.presentation.screens.calendar.CalendarScreen
 import com.example.transervmedical.presentation.screens.home.HomeScreen
 import com.example.transervmedical.presentation.screens.splash.SplashScreen
 
+@ExperimentalFoundationApi
 @Composable
 fun Routes(navHostController: NavHostController) {
 
@@ -19,6 +22,9 @@ fun Routes(navHostController: NavHostController) {
         }
         composable(route = Screen.Home.route) {
             HomeScreen(navHostController = navHostController)
+        }
+        composable(route = Screen.Calendar.route) {
+            CalendarScreen()
         }
     }
 
